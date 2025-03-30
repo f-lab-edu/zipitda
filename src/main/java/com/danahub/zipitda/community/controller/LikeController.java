07f1267feda707e34cadffbe18cd28f2002013f7
Daeun Kim <74163296+abcdana@users.noreply.github.com>
2025-03-30 17:39:57 +0900

    feat: 좋아요 추가 삭제, 좋아요 수 조회

--- src/main/java/com/danahub/zipitda/community/controller/LikeController.java
+++ src/main/java/com/danahub/zipitda/community/controller/LikeController.java
@@ -1,4 +1,35 @@
 package com.danahub.zipitda.community.controller;
 
+import com.danahub.zipitda.common.dto.CommonResponse;
+import com.danahub.zipitda.community.dto.LikeRequestDto;
+import com.danahub.zipitda.community.service.LikeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/community/likes")
+@RequiredArgsConstructor
+@Tag(name = "Like", description = "좋아요 관리 API")
 public class LikeController {
+
+    private final LikeService likeService;
+
+    @PostMapping
+    @Operation(summary = "좋아요 API", description = "좋아요를 클릭합니다.")
+    public void likePost(@RequestBody LikeRequestDto requestDto) {
+        likeService.likePost(requestDto.userId(), requestDto.postId());
+    }
+    @DeleteMapping
+    @Operation(summary = "좋아요 해제 API", description = "좋아요를 해제합니다.")
+    public void unlikePost(@RequestBody LikeRequestDto requestDto) {
+        likeService.unlikePost(requestDto.userId(), requestDto.postId());
+    }
+
+    @GetMapping
+    @Operation(summary = "좋아요 수 조회 API", description = "해당 포스트의 좋아요 수를 조회합니다.")
+    public CommonResponse<Long> countPostLikes(@RequestParam Long postId) {
+        return CommonResponse.success(likeService.countPostLikes(postId));
+    }
 }
